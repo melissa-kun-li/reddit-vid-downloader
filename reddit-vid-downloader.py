ccc97f803097c5539e7da0ecec5f4bc04afb444a
Melissa Li <li.melissa.kun@gmail.com>
2020-12-29 17:00:25 -0500

    Use local path instead of absolute

--- reddit-vid-downloader.py
+++ reddit-vid-downloader.py
@@ -2,12 +2,11 @@ import requests
 import wget
 import subprocess
 import os
-import pathlib
 
 from urllib.error import HTTPError
 
 def reddit_vid_downloader():
-    url = input('Please enter the Reddit link which has the Reddit embedded video: ')
+    url = input('Please enter the Reddit link which has the Reddit hosted video: ')
     info = {'user-agent':'vdownloader by /u/panaora'}
     response = requests.get(url + '.json', headers = info)
     data = response.json()
@@ -16,35 +15,20 @@ def reddit_vid_downloader():
     title = part[0]['data']['title'] # used in renaming file
     video_link = part[0]['data']['secure_media']['reddit_video']['fallback_url'][:-16] # https://...DASH_1080.mp4 or DASH_240, etc.
     audio_link = video_link.split('_')[0] + '_audio.mp4' # https://...DASH_audio.mp4
-    
-    script_path = pathlib.Path(__file__).parent.absolute()
 
-    wget.download(video_link) # /Users/melissali/Documents/reddit_vid_downloader/DASH_1080.mp4
+    wget.download(video_link) 
     DASH_video = 'DASH' + video_link.split('/DASH')[1] # works for diff DASH_xxx or DASH_1080 
-    old_video = os.path.join(script_path, DASH_video)
 
     try:
-        wget.download(audio_link) # /Users/melissali/Documents/reddit_vid_downloader/DASH_audio.mp4
+        wget.download(audio_link)
     except HTTPError: # Reddit video has no audio so DASH_audio.mp4 DNE
-        new_video = os.path.join(script_path, title + '.mp4')
-        os.rename(old_video, new_video) # rename the Reddit video file
+        os.rename(DASH_video, title + '.mp4') 
         return
 
-    new_video = os.path.join(script_path, 'video.mp4')
-    os.rename(old_video, new_video)
-
-    # merge the video and audio file
+    os.rename(DASH_video, 'video.mp4')
     subprocess.run('ffmpeg -i video.mp4 -i DASH_audio.mp4 -map 0:v -map 1:a -c copy reddit_vid.mp4', shell = True)
-
-    # delete the old DASH video and DASH_audio files
-    path_video = os.path.join(script_path, 'video.mp4')
-    path_audio = os.path.join(script_path, 'DASH_audio.mp4')
-    os.remove(path_video)
-    os.remove(path_audio)
-
-    # rename the merged Reddit video + audio file 
-    old_file = os.path.join(script_path, 'reddit_vid.mp4')
-    new_file = os.path.join(script_path, title +'.mp4')
-    os.rename(old_file,new_file)
+    os.remove('video.mp4')
+    os.remove('DASH_audio.mp4')
+    os.rename('reddit_vid.mp4', title + '.mp4')
 
 reddit_vid_downloader()
